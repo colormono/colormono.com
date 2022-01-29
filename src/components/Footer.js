1bb6134d592d7acbbe263016296e9fd58831fa88
Mariano Rivas <colormono@gmail.com>
2022-01-28 23:27:08 -0300

    feat: update footer styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,9 @@ import NowPlaying from './NowPlaying';
 
 export default function Footer() {
   return (
-    <footer className="bg-white dark:bg-gray-900 text-white">
-      <div className="w-full p-8 lg:p-16 max-w-[1280px] mx-auto flex justify-between">
-        <div className="flex space-x-4 mb-4">
+    <footer>
+      <div className="w-full p-8 lg:p-16 max-w-[1280px] mx-auto flex flex-col md:flex-row md:justify-between md:items-center gap-10">
+        <div className="flex space-x-4">
           <ExternalLink href="https://instagram.com/colormono">
             <InstagramIcon />
           </ExternalLink>
@@ -20,11 +20,11 @@ export default function Footer() {
           <ExternalLink href="https://www.linkedin.com/in/colormono">
             <LinkedinIcon />
           </ExternalLink>
-          <ExternalLink href="mailto:hello@colormono.com">
+          {/* <ExternalLink href="mailto:hello@colormono.com">
             <EmailIcon />
-          </ExternalLink>
+          </ExternalLink> */}
         </div>
-        <div className="space-x-3">
+        <div className="space-x-3 flex-grow">
           <ExternalLink href="https://photos.colormono.com">/photos</ExternalLink>
           {/* <Link href="/snippets">
           <a className="text-sm text-gray-500 hover:text-gray-600">/snippets</a>
--- src/components/icons/Email.js
+++ src/components/icons/Email.js
@@ -2,13 +2,7 @@ const EmailIcon = ({ href, children }) => (
   <>
     <span className="sr-only">Email</span>
     <svg className="h-5 w-5" viewBox="0 0 24 24">
-      <g
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      >
+      <g fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
         <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" />
         <polyline points="22,6 12,13 2,6" />
       </g>
