f6cfc080129fa9a922c14e1a97280f1a0c046e51
colormono <colormono@gmail.com>
2023-08-07 09:46:18 -0300

    feat: update design system

--- components/tailwind-indicator.tsx
+++ components/tailwind-indicator.tsx
@@ -2,7 +2,7 @@ export function TailwindIndicator() {
   if (process.env.NODE_ENV === "production") return null
 
   return (
-    <div className="fixed bottom-10 left-0 z-50 flex h-6 items-center justify-center bg-gray-800 p-3 font-mono text-xs uppercase text-white ">
+    <div className="fixed bottom-0 left-0 z-50 flex h-6 items-center justify-center bg-muted p-3 font-mono text-xs uppercase text-muted-foreground">
       <div className="block sm:hidden">xs</div>
       <div className="hidden sm:block md:hidden">sm</div>
       <div className="hidden md:block lg:hidden">md</div>
