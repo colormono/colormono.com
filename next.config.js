c7a67bcfb403426c0675f2d43b0886920121f76f
colormono <colormono@gmail.com>
2023-12-19 23:47:07 -0300

    ☠️: ignore eslint and typescript on build

--- next.config.js
+++ next.config.js
@@ -53,6 +53,18 @@ const nextConfig = {
       },
     ];
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
 
 module.exports = withContentlayer(nextConfig);
